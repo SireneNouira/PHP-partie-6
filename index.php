c62a653c9a4a28a5b6c21f384930d97ef8f39a5c
SireneNouira <sirene.nouira16@gmail.com>
2024-12-05 15:02:46 +0100

    fièn-ub

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+
+//EXERCICE 1
+
+//   // Vérification si les paramètres existent
+//   if (isset($_GET['lastname']) && isset($_GET['firstname'])) {
+//     // Affichage des paramètres
+//     echo "<p>Nom : " . htmlspecialchars($_GET['lastname']) . "</p>";
+//     echo "<p>Prénom : " . htmlspecialchars($_GET['firstname']) . "</p>";
+// } else {
+//     // Message d'erreur si les paramètres sont absents
+//     echo "<p>Les paramètres 'lastname' et 'firstname' ne sont pas présents dans l'URL.</p>";
+// }
+
+//EXERCICE 2
+
+
+// if (isset($_GET['age'])) {
+//     echo "<p>Age: " . htmlspecialchars($_GET['age']) . "</p>";
+
+// }else{
+//     echo "<p>Le paramètres 'age'  n'est' pas présent dans l'URL.</p>";
+// }
+
+//EXERCICE 3
+
+//   // Vérification si les paramètres existent
+//   if (isset($_GET['startDate']) && isset($_GET['endDate'])) {
+//     // Affichage des paramètres
+//     echo  htmlspecialchars($_GET['startDate']) . "</p>";
+//     echo  htmlspecialchars($_GET['endDate']) ;
+// } else {
+//     // Message d'erreur si les paramètres sont absents
+//     echo "<p>Les paramètres 'startDate' et 'endDate' ne sont pas présents dans l'URL.</p>";
+// }
+
+//EXERCICE 4
+
+//   // Vérification si les paramètres existent
+//   if (isset($_GET['language']) && isset($_GET['server'])) {
+//     // Affichage des paramètres
+//     echo  htmlspecialchars($_GET['language']) . "</p>";
+//     echo  htmlspecialchars($_GET['server']) ;
+// } else {
+//     // Message d'erreur si les paramètres sont absents
+//     echo "<p>Les paramètres 'language' et 'server' ne sont pas présents dans l'URL.</p>";
+// }
+
+//EXERCICE 5
+
+
+// if (isset($_GET['week'])) {
+//     echo "<p>week: " . htmlspecialchars($_GET['week']) . "</p>";
+
+// }else{
+//     echo "<p>Le paramètres 'week'  n'est' pas présent dans l'URL.</p>";
+// }
+
+
+//EXERCICE 6
+
+//   // Vérification si les paramètres existent
+//   if (isset($_GET['building']) && isset($_GET['room'])) {
+//     // Affichage des paramètres
+//     echo  htmlspecialchars($_GET['building']) . "</p>";
+//     echo  htmlspecialchars($_GET['room']) ;
+// } else {
+//     // Message d'erreur si les paramètres sont absents
+//     echo "<p>Les paramètres 'building' et 'room' ne sont pas présents dans l'URL.</p>";
+// }
